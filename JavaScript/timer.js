32326108678d66955494f4204032e97d71419d5b
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-10-15 14:24:24 +0300

    created file timer.js
    it's counting time from entered value down to 00:00

---
+++ JavaScript/timer.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let secs, mins;
+
+const readline = require('readline');
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Set timer: ', (answer) => { // Set the number of minutes and seconds  
+  const date = answer.split(':');        // to be counted to
+  rl.close()                             // separeted by a colon
+  mins = parseInt(date[0]);
+  secs = parseInt(date[1]);
+  console.log(`Timer set for ${mins} minute(s) and ${secs} second(s)`);
+  timerOn();
+});
+
+const timerOn = () => { 
+  setInterval(() => timer(), 1000);
+};
+
+function timer() {
+  if ((mins > 9) && (secs > 9)) {
+    writeTimeLeft('','');
+    secs--;
+  } else if ((mins > 9) && (secs > -1)) {
+    writeTimeLeft('','0');
+    secs--;
+    if (secs === -1) {
+      mins--;
+      secs = 59;
+    };     
+  } else if ((mins > -1) && (secs > 9)){
+    writeTimeLeft('0','');
+    secs--;
+  } else if ((mins > -1) && (secs > -1)){
+    writeTimeLeft('0','0');
+    secs--;
+    if (secs === -1) {
+      mins--;
+      secs = 59;
+    };
+  };
+}
+
+function writeTimeLeft(zeroMins, zeroSecs) {
+  process.stdout.write(
+  '\b\b\b\b\b' + `${zeroMins}${mins}:${zeroSecs}${secs}`
+  );
+}
