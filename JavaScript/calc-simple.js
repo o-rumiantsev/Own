e611102ba2f5a0c580a4a8588f8565849f327702
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-10-18 16:33:48 +0300

    update calc-simple.js

--- JavaScript/calc-simple.js
+++ JavaScript/calc-simple.js
@@ -15,46 +15,8 @@ function calculationLoop() {
       rl.close()
     } else {
       console.log(eval(answer));
-    //  const expr = answer.split(' ');
-      rl.pause();
-  //    doOperation(expr[0], expr[1], expr[2]);
+      rl.pause()
       calculationLoop();
     }
   })
-}
-
-/*function doOperation(a, operator, b) {
-  const operand1 = parseFloat(a);
-  const operand2 = parseFloat(b);
-  switch (operator) {
-    case '+':
-      Ops.add(operand1, operand2);
-      break;
-    case '-':
-      Ops.subtr(operand1, operand2);
-      break;
-    case '*':
-      Ops.mult(operand1, operand2);
-      break;
-    case '/':
-      Ops.div(operand1, operand2);
-      break;
-    case '%':
-      Ops.divRest(operand1, operand2);
-      break;
-    case '^':
-      Ops.power(operand1, operand2);
-      break;
-    default:
-      console.log(`Error: ${operator} is not a symbol of operation`);
-  }
-}
-
-const Ops = {
-  add: (a, b) => console.log(a + b),
-  subtr: (a, b) => console.log(a - b) ,
-  mult: (a, b) => console.log(a * b),
-  div: (a, b) => console.log(a / b),
-  divRest: (a, b) => console.log(Math.floor(a / b) + ' rest: ' + (a % b)),
-  power: (a, b) => console.log(Math.pow(a, b))
-};*/
+};
