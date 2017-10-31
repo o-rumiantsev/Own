e87f5d0cbcae2c3c3351fd480fd70af1db79ab63
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-10-31 08:11:43 +0200

    optimized sortGroup.deleteSimilar.js

--- JavaScript/McCluskey/sortGroup.js
+++ JavaScript/McCluskey/sortGroup.js
@@ -3,37 +3,42 @@
 //exports to iteration.js
 exports.deleteSimilar = (Obj) => {
 
-  for (const i in Obj) {
-    if (Obj[i] !== undefined) {
-      const amountOfImplics = Obj[i].length;
-      for (let j = 0; j < amountOfImplics - 1; ++j) {
-        for (let k = j + 1; k < amountOfImplics; ++k) {
-          if (Obj[i][j] === Obj[i][k]) {
-            Obj[i].splice(k, 1);
+    for (const i in Obj) {
+      if (Obj[i] !== undefined) {
+        let amountOfImplics = Obj[i].length;
+        for (let j = 0; j < amountOfImplics - 1; ++j) {
+          const currentStr = Obj[i][j];
+          const condition1 = currentStr !== undefined
+          for (let k = j + 1; k < amountOfImplics; ++k) {
+            const comparingStr = Obj[i][k];
+            const condition2 = comparingStr !== undefined
+            if (currentStr === comparingStr && condition1 && condition2) {
+              Obj[i].splice(k, 1);
+              exports.deleteSimilar(Obj);
+            }
           }
         }
       }
     }
-  }
 
-  // Recheck, to find similar implicants, which hadn't been deleted
-  for (const i in Obj) {
-    if (Obj[i] !== undefined) {
-      const amountOfImplics = Obj[i].length;
-      for (let j = 0; j < amountOfImplics - 1; ++j) {
-        const currentStr = Obj[i][j];
-        for (let k = j + 1; k < amountOfImplics; ++k) {
-          const comparingStr = Obj[i][k];
-          if (currentStr === comparingStr) {
-            exports.deleteSimilar(Obj);
-          }
-        }
-      }
-    }
-  }
+    // Recheck, to find similar implicants, which hadn't been deleted
+    // for (const i in Obj) {
+    //   if (Obj[i] !== undefined) {
+    //     const amountOfImplics = Obj[i].length;
+    //     for (let j = 0; j < amountOfImplics - 1; ++j) {
+    //       const currentStr = Obj[i][j];
+    //       for (let k = j + 1; k < amountOfImplics; ++k) {
+    //         const comparingStr = Obj[i][k];
+    //         if (currentStr === comparingStr) {
+    //           exports.deleteSimilar(Obj);
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
 
-  return Obj;
-};
+    return Obj;
+  };
 
 // exports to separation.js
 exports.checkBeenUsed = (arr) => {
