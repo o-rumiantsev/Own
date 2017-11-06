87e1f26f684451a7efae7e53b64e62514719c74c
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-11-06 22:25:01 +0200

    improved timer.js

--- JavaScript/timer.js
+++ JavaScript/timer.js
@@ -9,18 +9,22 @@ let rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Set timer: ', (answer) => { // Set the number of minutes and seconds  
+rl.question('Set timer: ', (answer) => { // Set the number of minutes and seconds
   const date = answer.split(':');        // to be counted to
   rl.close()                             // separeted by a colon
   mins = parseInt(date[0]);
   secs = parseInt(date[1]);
   console.log(`Timer set for ${mins} minute(s) and ${secs} second(s)`);
-  timerOn();
+  timeMachine.timerOn();
 });
 
-const timerOn = () => { 
-  setInterval(() => timer(), 1000);
-};
+const timeMachine = {
+  timerOn() {
+    this.interval = setInterval(() => timer(), 1000);
+  }
+}
+
+timer = timer.bind(timeMachine);
 
 function timer() {
   if ((mins > 9) && (secs > 9)) {
@@ -32,11 +36,18 @@ function timer() {
     if (secs === -1) {
       mins--;
       secs = 59;
-    };  } else if ((mins > -1) && (secs > 9)){
+    };
+  } else if ((mins > -1) && (secs > 9)){
     writeTimeLeft('0','');
     secs--;
   } else if ((mins > -1) && (secs > -1)){
     writeTimeLeft('0','0');
+    if (mins === 0 && secs === 0) {
+      clearInterval(this.interval);
+      this.interval = null;
+      console.log('\nTIME IS OUT!');
+      return;
+    };
     secs--;
     if (secs === -1) {
       mins--;
