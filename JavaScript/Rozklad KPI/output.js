ef23fd3122ea87d7a56d81c2421e7b181ae1b1d0
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-11-17 06:20:08 +0200

    days of the week

--- JavaScript/Rozklad KPI/output.js
+++ JavaScript/Rozklad KPI/output.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const Week = {
+  '1': 'Понеділок',
+  '2': 'Вівторок',
+  '3': 'Середа',
+  '4': 'Четвер',
+  '5': 'П\'ятниця',
+  '6': 'Субота'
+};
+
 // getting week: {1, 2}
 
 Date.prototype.getWeek = function() {
@@ -14,25 +23,34 @@ const week = 2 - (new Date().getWeek() % 2);
 // output
 
 exports.writeTimetable = (data) => {
-  if (day >= 6) day = 1;
+  if (6 <= day) day = 1;
   else day++;
-  const tomorrow = data[week][day];
+
+  let tomorrow = data[week][day];
+  if (!tomorrow) {
+    day = 1;
+    tomorrow = data[week][day];
+  }
+
+  console.log(`\n\x1b[1;37;3m${Week[day]}\x1b[23m`);
+
+  let order, name, type, rooms, building, teachers;
   for (const i in tomorrow) {
-    const order = i;
-    const name = tomorrow[i]['discipline']['name'];
-    let type = tomorrow[i]['type'];
+    order = i;
+    name = tomorrow[i]['discipline']['name'];
+    type = tomorrow[i]['type'];
     type = type === 0 ? 'Лекція' :
            type === 1 ? 'Практика' :
            type === 2 ? 'Лабораторна' : '';
-    let rooms = tomorrow[i]['rooms'];
-    let building = '';
+    rooms = tomorrow[i]['rooms'];
+    building = '';
     if (rooms[0]) {
       rooms = `${rooms.map(room => room['name'])
                    .toString()
                    .replace(/,/g, '$& ')}-`;
       building = tomorrow[i]['rooms'][0]['building']['name'];
     } else rooms = '';
-    let teachers = tomorrow[i]['teachers'];
+    teachers = tomorrow[i]['teachers'];
     if (teachers[0]) {
       teachers = teachers.map(teacher => teacher['full_name'])
                          .toString()
