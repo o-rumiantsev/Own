2f7604f39962cc1539bb770d1eeb9269f32a4e09
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-11-23 12:03:05 +0200

    async-notasync?

--- JavaScript/Xmpl/callback-notcallback.js
+++ JavaScript/Xmpl/callback-notcallback.js
@@ -5,14 +5,14 @@ const fs = require('fs');
 const add = (a, b) => a + b;
 const sum = (a, b, callback) => callback(a, b);
 
-fs.readFile('./timer.js', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile('./timer.js', (err, data) => {
+//   if (err) throw err;
+//
+// });
 
 sum(5, 2, (a, b) => {
   let res = 0;
-  for (let i = 0; i < 100000; ++i) {
+  for (let i = 0; i < 10000; ++i) {
     res += a + b;
   }
   console.log('Use sum: ' + res);
